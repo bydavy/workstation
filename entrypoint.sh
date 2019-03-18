5f21f15a0f1f8151b9a80699f2a765b770673261
Davy Leggieri <davy.leggieri@gmail.com>
2019-03-17 23:17:46 -0700

    renamed directory for dotfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ~/code/dotfiles ]; then
+if [ ! -d ~/.dotfiles ]; then
   echo "Cloning dotfiles"
-  mkdir -p ~/code
-  cd ~/code
-  git clone https://github.com/bydavy/dotfiles.git
-  cd ~/code/dotfiles
+  git clone https://github.com/bydavy/dotfiles.git .dotfiles
+  cd ~/.dotfiles
   git remote set-url origin git@github.com:bydavy/dotfiles.git
 fi
 
-cd ~/code/dotfiles 
+cd ~/.dotfiles
 ./setup.sh
 cd
 . .bashrc
